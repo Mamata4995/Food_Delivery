2b1f28bea65b8ae4efb7f25f212f7b7c4bf5508c
mamata <momo@gmail.com>
2022-03-08 01:50:53 +0530

    Done with uploading

---
+++ Delivery/restaurant/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls import url,include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import Dashboard, OrderDetails
+
+urlpatterns = [
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('orders/<int:pk>/', OrderDetails.as_view(), name='order-details'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
